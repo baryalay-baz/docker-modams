5bc06199e19e7301b1d18b2728d8fa6673bcbfd4
baryalayb <BaryalayB@unops.org>
2023-05-17 13:15:37 +0300

    Add Identity

--- MODAMS.DataAccess/Data/ApplicationDbContext.cs
+++ MODAMS.DataAccess/Data/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 //using
 
 namespace MODAMS.DataAccess.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
