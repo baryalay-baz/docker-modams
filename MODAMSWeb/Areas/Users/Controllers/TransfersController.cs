a1c10e7eeeb2baa49f8d59b7b6e7e3342c4147d5
baryalayb <BaryalayB@unops.org>
2023-08-02 01:04:13 +0300

    Transfers Module started

---
+++ MODAMSWeb/Areas/Users/Controllers/TransfersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MODAMS.DataAccess.Data;
+using MODAMS.Utility;
+
+namespace MODAMSWeb.Areas.Users.Controllers
+{
+    [Area("Users")]
+    [Authorize]
+    public class TransfersController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IAMSFunc _func;
+        private int _employeeId;
+
+        public TransfersController(ApplicationDbContext db, IAMSFunc func)
+        {
+            _db = db;
+            _func = func;
+            _employeeId = _func.GetEmployeeId(); 
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
