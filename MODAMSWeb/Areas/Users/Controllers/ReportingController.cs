8068cbf2e0d8b9c76d56ef64071ba3ec7090784d
baryalay@msn.com <baryalay.baz@gmail.com>
2025-05-19 15:40:18 +0300

    Forgot password functionality localized

--- MODAMSWeb/Areas/Users/Controllers/ReportingController.cs
+++ MODAMSWeb/Areas/Users/Controllers/ReportingController.cs
@@ -7,13 +7,8 @@ using MODAMS.Models;
 using MODAMS.Models.ViewModels;
 using MODAMS.Models.ViewModels.Dto;
 using MODAMS.Utility;
-using NuGet.ContentModel;
-using Telerik.Reporting;
-using Telerik.Reporting.Processing;
+using System.Globalization;
 using Telerik.Reporting.Services;
-using Telerik.Reporting.Services.AspNetCore;
-using Telerik.Reporting.Services.Engine;
-
 
 namespace MODAMSWeb.Areas.Users.Controllers
 {
@@ -26,6 +21,8 @@ namespace MODAMSWeb.Areas.Users.Controllers
         private int _employeeId;
         private int _supervisorEmployeeId;
         private int _storeId;
+
+        private readonly bool _isSomali;
         private readonly IWebHostEnvironment _webHostEnvironment;
         private readonly IReportSourceResolver _reportResolver;
         public ReportingController(ApplicationDbContext db, IAMSFunc func, IWebHostEnvironment webHostEnvironment, IReportSourceResolver reportResolver)
@@ -36,15 +33,13 @@ namespace MODAMSWeb.Areas.Users.Controllers
             _supervisorEmployeeId = _func.GetSupervisorId(_employeeId);
             _webHostEnvironment = webHostEnvironment;
             _reportResolver = reportResolver;
+
+            _isSomali = CultureInfo.CurrentUICulture.Name == "so";
         }
 
         [HttpGet]
         public async Task<IActionResult> Index()
         {
-            //var assets = _db.Assets.Include(m => m.SubCategory).ThenInclude(m => m.Category)
-            //    .Include(m => m.Store).ThenInclude(m => m.Department)
-            //    .Include(m => m.AssetStatus).Include(m => m.Condition).ToList();
-
             ReportingDTO dto = new ReportingDTO();
             dto = await PopulateDTO(dto);
 
@@ -98,34 +93,35 @@ namespace MODAMSWeb.Areas.Users.Controllers
 
             var storeSelectList = stores.ToList().Select(m => new SelectListItem
             {
+                Text = _isSomali ? m.NameSo : m.Name,
+                Value = m.Id.ToString()
+            });
+            var categories = await _db.Categories.Select(m => new SelectListItem
+            {
+                Text = _isSomali ? m.CategoryNameSo : m.CategoryName,
+                Value = m.Id.ToString()
+            }).ToListAsync();
+
+            var subCategories = await _db.SubCategories.Select(m => new SelectListItem
+            {
+                Text = _isSomali ? m.SubCategoryNameSo : m.SubCategoryName,
+                Value = m.Id.ToString()
+            }).ToListAsync();
+            var assetStatuses = await _db.AssetStatuses.Select(m => new SelectListItem
+            {
+                Text = _isSomali ? m.StatusNameSo : m.StatusName,
+                Value = m.Id.ToString()
+            }).ToListAsync();
+            var assetConditions = await _db.Conditions.Select(m => new SelectListItem
+            {
+                Text = _isSomali ? m.ConditionNameSo : m.ConditionName,
+                Value = m.Id.ToString()
+            }).ToListAsync();
+            var donors = await _db.Donors.Select(m => new SelectListItem
+            {
                 Text = m.Name,
                 Value = m.Id.ToString()
-            });
-            var categories = _db.Categories.ToList().Select(m => new SelectListItem
-            {
-                Text = m.CategoryName,
-                Value = m.Id.ToString()
-            });
-            var subCategories = _db.SubCategories.ToList().Select(m => new SelectListItem
-            {
-                Text = m.SubCategoryName,
-                Value = m.Id.ToString()
-            });
-            var assetStatuses = _db.AssetStatuses.ToList().Select(m => new SelectListItem
-            {
-                Text = m.StatusName,
-                Value = m.Id.ToString()
-            });
-            var assetConditions = _db.Conditions.ToList().Select(m => new SelectListItem
-            {
-                Text = m.ConditionName,
-                Value = m.Id.ToString()
-            });
-            var donors = _db.Donors.ToList().Select(m => new SelectListItem
-            {
-                Text = m.Name,
-                Value = m.Id.ToString()
-            });
+            }).ToListAsync();
 
             dto.AssetStores = storeSelectList;
             dto.AssetStatuses = assetStatuses;
@@ -137,25 +133,25 @@ namespace MODAMSWeb.Areas.Users.Controllers
             //Populate Transfer Report
             dto.TransferStores = allStores.ToList().Select(m => new SelectListItem
             {
-                Text = m.Name,
+                Text = _isSomali ? m.NameSo : m.Name,
                 Value = m.Id.ToString()
             });
-            dto.TransferStatuses = _db.TransferStatuses.ToList().Select(m => new SelectListItem
+            dto.TransferStatuses = await _db.TransferStatuses.Select(m => new SelectListItem
             {
-                Text = m.Status,
+                Text = _isSomali ? m.StatusSo : m.Status,
                 Value = m.Id.ToString()
-            });
+            }).ToListAsync();
 
             //Populate Disposal= Report
             var disposalTypes = await _db.DisposalTypes.ToListAsync();
             dto.DisposalTypes = disposalTypes.ToList().Select(m => new SelectListItem
             {
-                Text = m.Type,
+                Text = _isSomali ? m.TypeSo : m.Type,
                 Value = m.Id.ToString()
             });
 
             return dto;
         }
-        
+
     }
 }
